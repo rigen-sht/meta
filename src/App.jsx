52d49d6d19cf4670ef00bb7c514d5c8ac16eda74
rigen <rigen.sht@gmail.com>
2023-08-24 08:20:20 +0545

    updated

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  const currentUser = true;
+  return (
+    <BrowserRouter>
+      <Topbar />
+      <Routes>
+        <Route exact path="/"></Route>
+        <Route path="/posts"></Route>
+        <Route path="/register">
+          {currentUser ? <Homepage /> : <Register />}
+        </Route>
+        <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">{currentUser ? <Settings /> : <Login />}</Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
